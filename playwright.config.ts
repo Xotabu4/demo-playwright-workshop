9fee3707522544596ae2c53293830ba16ef55f1a
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2023-09-24 18:26:35 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     baseURL: 'https://shopdemo-alex-hot.koyeb.app',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
   },
   /* Configure projects for major browsers */
   projects: [
