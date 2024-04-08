88e6f69de611431c64644b4c1a8868a39a545b81
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2024-04-08 09:48:36 +0300

    1.43

--- tests/examples/capturing-elements.test.ts
+++ tests/examples/capturing-elements.test.ts
@@ -1,24 +1,30 @@
-import { expect, test } from '@playwright/test';
+import { test } from "@playwright/test";
 
-
-
-test('Capturing captcha popup', {
-    tag: ['@test', '@JIRAID=123123'],
+test(
+  "Capturing captcha popup",
+  {
+    tag: ["@test", "@JIRAID=123123"],
     annotation: [
-        { type: 'issue', description: '123123' },
-        { type: 'issue', description: '123123' },
-        { type: 'issue', description: '123123' },
-    ]
-  }, async ({ page }) => {
+      { type: "issue", description: "123123" },
+      { type: "issue", description: "123123" },
+      { type: "issue", description: "123123" },
+    ],
+  },
+  async ({ page }) => {
     // Setup the handler.
-    await page.addLocatorHandler(
-        page.getByRole('alertdialog'),
-        async () => {
-            console.log('cookie popup found')
-            await page.getByRole('button', { name: 'Allow All' }).click();
-        });
+    await page.addLocatorHandler(page.getByRole("alertdialog"), async () => {
+      console.log("cookie popup found");
+      await page.getByRole("button", { name: "Allow All" }).click();
+    });
     // Write the test as usual.
-    await page.goto('https://www.cookiepro.com/knowledge/how-do-i-test-cookie-blocking-implementation/');
-    await page.locator('#maincontent').getByRole('link', { name: 'Pricing' }).click();
+    await page.goto(
+      "https://www.cookiepro.com/knowledge/how-do-i-test-cookie-blocking-implementation/"
+    );
+    await page.pause();
+    await page
+      .locator("#maincontent")
+      .getByRole("link", { name: "Pricing" })
+      .click();
     // await page.waitForTimeout(60000);
-})
+  }
+);
